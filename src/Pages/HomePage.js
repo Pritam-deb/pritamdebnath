0171b3608a05eb98913bfb0b15f836f9a06ba853
Pritam <pritamdebnath297@gmail.com>
2021-06-02 21:03:46 +0530

    feat: added facebook icon

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 
 const HomePage = () => {
@@ -7,8 +9,13 @@ const HomePage = () => {
             <div className="hero">
                 <h1 className="hero-text">
                     Hi, I am 
-                    <span>Pritam</span>
+                    <span> Pritam</span>
                 </h1>
+                <p className="h-sub-text">I am a self taught web developer. ReactJS is my primary tech stack.
+                I also love combat sports, gaming and anime. ϞϞ(๑⚈ ․̫ ⚈๑)∩</p>
+                <div className="icons">
+                    <FontAwesomeIcon icon={faFacebook}/>
+                </div>
             </div>
         </div>
     )
