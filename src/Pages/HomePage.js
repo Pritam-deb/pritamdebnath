1e1e0c36c75a907d337bde8e85e10feea7142229
Pritam <pritamdebnath297@gmail.com>
2021-06-02 13:52:40 +0530

    style: home page, hero section

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,7 +3,14 @@ import React from 'react';
 
 const HomePage = () => {
     return(
-        <div>HomePage</div>
+        <div className="HomePage">
+            <div className="hero">
+                <h1 className="hero-text">
+                    Hi, I am 
+                    <span>Pritam</span>
+                </h1>
+            </div>
+        </div>
     )
 }
 
