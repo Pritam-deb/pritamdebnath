0e58e5e1be7894a21564f4a373472f7b460e96ea
Pritam <pritamdebnath297@gmail.com>
2021-06-04 18:55:24 +0530

    style: about section

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -11,10 +11,24 @@ const ImageSection = () => {
                     I am
                     <span> Pritam</span>
                 </h4>
-                <p>I have been coding ever since I was 16 and I felt it is the only field I could be best without anyone being an obstacle.</p>
+                <p className="about-text">I have been coding ever since I was 16 and I felt it is the only field I could be best without anyone being an obstacle.</p>
                 <div className="about-details">
-
+                    <div className="left-section">
+                        <p>Full Name</p>
+                        <p>Age</p>
+                        <p>Nationality</p>
+                        <p>Languages</p>
+                        <p>Skills</p>
+                    </div>
+                    <div className="right-section">
+                        <p> : Pritam Debnath</p>
+                        <p> : 23</p>
+                        <p> : Indian</p>
+                        <p> : English, Hindi, Bengali</p>
+                        <p> : Ruby | ReactJS | HTML | CSS | Python</p>
+                    </div>
                 </div>
+                <button className="btn">Download Resume</button>
             </div>
         </div>
     )
