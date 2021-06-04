13403247e83bb2c0fd650430994c862e66678ef8
Pritam <pritamdebnath297@gmail.com>
2021-06-04 20:45:45 +0530

    :-1:  skills bar

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,5 +1,6 @@
-import ImageSection from '../Components/ImageSection'
-import Title from '../Components/Title'
+import ImageSection from '../Components/ImageSection';
+import SkillsSection from '../Components/SkillsSection';
+import Title from '../Components/Title';
 
 
 const AboutPage = () => {
@@ -7,6 +8,15 @@ const AboutPage = () => {
         <div className="AboutPage">
             <Title title={"About Me"} span={"About ME"} />
             <ImageSection/>
+            <Title title={"My Skills"} span={"My Skills"} />
+            <div className="skills-container">
+                <SkillsSection skill={'ReactJS'} progress={'70%'} width={'70%'}/>
+                <SkillsSection skill={'JS'} progress={'75%'} width={'75%'}/>
+                <SkillsSection skill={'HTML'} progress={'80%'} width={'80%'}/>
+                <SkillsSection skill={'CSS'} progress={'60%'} width={'60%'}/>
+                <SkillsSection skill={'Ruby'} progress={'40%'} width={'40%'}/>
+            </div>
+            
         </div>
     )
 }
