e71115ce36a65f3f84ea025fa1a88d4a9c912435
Pritam <pritamdebnath297@gmail.com>
2021-06-05 16:03:12 +0530

    feat: added blogs page

---
+++ src/Components/allBlogs.js
@@ -0,0 +1,39 @@
+import blog1 from '../img/blogs/blog1.svg';
+import blog2 from '../img/blogs/blog2.svg';
+import blog3 from '../img/blogs/blog3.svg';
+// import blog4 from '../img/blogs/blog4.svg';
+// import blog5 from '../img/blogs/blog5.svg';
+
+const blogs = [
+    {
+        id: 1,
+        image: blog1,
+        title: 'A month in ReacyJS',
+        link: 'https://p2builds.hashnode.dev/a-month-in-reactjs',
+        date : '13',
+        month: 'May',
+        year: '2021'
+    },
+    {
+        id: 2,
+        image: blog2,
+        title: 'Hooking up with useState and useEffect',
+        link: 'https://p2builds.hashnode.dev/hooking-up-with-usestate-and-useeffect',
+        date : '15',
+        month: 'May',
+        year: '2021'
+    },
+    {
+        id: 3,
+        image: blog3,
+        title: 'Components in ReactJS',
+        link: 'https://p2builds.hashnode.dev/components-in-reactjs',
+        date : '15',
+        month: 'May',
+        year: '2021'
+    },
+   
+];
+
+
+export default blogs;
--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,12 +1,30 @@
 
+import Title from '../Components/Title';
+import allBlogs from '../Components/allBlogs';
 
-
-const BlogsPage = () =>{
-    return(
-        <div className="BlogsPage">
-            <h1>Blog Page</h1>
+const BlogsPage = () => {
+    return (
+        <div >
+            <div className="b-title">
+                <Title title={'Recent Blogs'} span={'Recent Blogs'} />
+            </div>
+            <div className="BlogsPage">
+                {
+                    allBlogs.map((blog)=>{
+                        return <div className="blog" key={blog.id}>
+                            <div className="blog-content">
+                                
+                                <a href={blog.link} className="blog-link">
+                                <img src={blog.image} alt=""/>
+                                    {blog.title}
+                                </a>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+            
         </div>
     )
 }
-
 export default BlogsPage;
