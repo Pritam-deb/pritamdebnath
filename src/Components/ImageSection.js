902aad3532e64d71c778f9b8eb6baa3e69d8bc83
Pritam <pritamdebnath297@gmail.com>
2021-06-05 17:13:55 +0530

    :-1: resume  button

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -28,7 +28,7 @@ const ImageSection = () => {
                         
                     </div>
                 </div>
-                <button className="btn"> <a href="https://drive.google.com/file/d/1l2MgIltOFi3D0UVD4geGnJ6o85M3FZfJ/view?usp=drivesdk"> Download Resume</a></button>
+                <button className="btn" href="https://drive.google.com/file/d/1l2MgIltOFi3D0UVD4geGnJ6o85M3FZfJ/view?usp=drivesdk"> Download Resume</button>
             </div>
         </div>
     )
